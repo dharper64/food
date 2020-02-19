ba5bf494da2a8c963dbbe9b07dac1cfd6695de15
David Harper <HarperD@ts.local>
2020-02-19 15:21:11 +0000

    New function getAreaWeather -  has errors.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import * as admin from 'firebase-admin'; //SDK
 admin.initializeApp()
 
  // Start writing Firebase Functions
@@ -16,9 +16,56 @@ admin.initializeApp()
 
  });
 
-export const getTestData = functions.https.onRequest((request, response) => {
-    //.region('europe-west2')
-    //.https.onRequest((request, response) => {
+ /*
+// Firestore trigger runs on update to document. https://www.youtube.com/watch?v=d9GrysWH1Lc
+export const getAreaWeather = 
+functions.https.onRequest((request,response) => {
+    admin.firestore().doc("areas/hampshire").get()
+    .then(areaSnapShot => {
+        const places = areaSnapShot.data().places
+        const promises = [] // create array
+        for (const place in places) {
+            const p = admin.firestore().doc(`weather/${place}`).get()
+            promises.push(p) // add data to array
+        }
+        
+        return Promise.all(promises) // When we have all data
+    })
+    .then(areaSnapShot => {
+        const results: (FirebaseFirestore.DocumentData | undefined)[] = []
+        areaSnapShot.forEach(placeSnap => {
+            const data = placeSnap.data()
+            data.place = placeSnap.id
+            results.push(data)
+        })
+        response.send(results)
+    })
+    .catch(error => {
+        console.log(error)
+        response.status(500).send(error)
+    })
+})
+*/
+/*
+// Firestore trigger runs on update to document. https://www.youtube.com/watch?v=652XeeKNHSk
+//  exmaple only, not tested.
+export const onHambleWeatherUpdate =
+functions.firestore.document('weather/hamble').onUpdate(change => {
+    const after = change.after.data()
+    const payLoad = {
+        data: {
+            temp: String(after.temp),
+            conditions: after.conditions
+        }
+    }
+    return admin.messaging().sendToTopic("weather_hamble", payLoad)
+})
+*/
+
+ // HTTP API end point function. https://www.youtube.com/watch?v=7IkUgCLr5oA
+export const getWeatherData = functions//.https.onRequest((request, response) => {
+    .region('europe-west2')
+    .https.onRequest((request, response) => {
 
     console.log('Hello!')
     
